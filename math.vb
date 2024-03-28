8214dd9444b6f331ad12348af0bc7ce35d43f5fe
William <willspri@gmail.com>
2024-03-28 13:27:47 +1300

    committed

---
+++ math.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+	Sub Main(args String())
+		Console.Write("Enter first number")
+		Dim num1 As Double = Convert.ToDouble(Console.ReadLine())
+	
+		Console.Write("Enter second number")
+		Dim num2 As Double = Convert.ToDouble)Console.ReadLine())
+	
+		Dim sum As Double = num1 + num2
+
+		Console.WriteLine($"Sum = {num1} and {num2} = {sum}")
+		console.WriteLine("Oress key to start...")
+		Console.ReadKey()
+	End Sub
+End Module
+	
